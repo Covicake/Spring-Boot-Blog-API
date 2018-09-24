6ae9488de9191b705aaa621c5e4f3bb8615671b0
Fernando <fernand-o.a@hotmail.com>
2018-09-25 00:05:27 +0100

    Added authentication filter for login and authorization filter for JWT check

---
+++ src/main/java/me/covicake/blog/Security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package me.covicake.blog.Security;
+
+public class SecurityConstants {
+    public static final String SECRET = "MySecretString";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final long EXPIRATION_TIME = 864_000_000;
+}
