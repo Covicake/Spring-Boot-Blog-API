8c0ee144d00825b65e207c0e201249e8036a58c7
Fernando <fernand-o.a@hotmail.com>
2018-09-25 00:13:26 +0100

    Added POST '/api/authors/sign-up' endpoint for registering new authors and GET '/api/authors' to get a list of all authors, also added AuthorService to manage the logic

---
+++ src/main/java/me/covicake/blog/Controllers/AuthorController.java
@@ -0,0 +1,27 @@
+package me.covicake.blog.Controllers;
+
+import me.covicake.blog.Entities.Author;
+import me.covicake.blog.Services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @PostMapping("/sign-up")
+    public void registerUser(@RequestBody Author author) {
+        authorService.registerAuthor(author);
+    }
+
+    @GetMapping("/")
+    public List<Author> getAuthors() {
+        return authorService.getAuthors();
+    }
+
+}
---
+++ src/main/java/me/covicake/blog/Services/AuthorService.java
@@ -0,0 +1,25 @@
+package me.covicake.blog.Services;
+
+import me.covicake.blog.Entities.Author;
+import me.covicake.blog.Repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public void registerAuthor(Author author) {
+        authorRepository.save(author);
+    }
+
+    public List<Author> getAuthors() {
+        return authorRepository.findAll().stream().collect(Collectors.toList());
+    }
+}
