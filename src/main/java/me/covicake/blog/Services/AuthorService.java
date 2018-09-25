2346e0e59eadadeb8152683c3ea540a9133e8620
Fernando <fernand-o.a@hotmail.com>
2018-09-25 18:21:01 +0100

    Removed passwords from response when querying Authors endpoint

--- src/main/java/me/covicake/blog/Services/AuthorService.java
+++ src/main/java/me/covicake/blog/Services/AuthorService.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
@@ -26,7 +26,13 @@ public class AuthorService {
     public Author findAuthorByUsername(String username) {
         return authorRepository.findByUsername(username);
     }
+
     public List<Author> getAuthors() {
-        return authorRepository.findAll().stream().collect(Collectors.toList());
+        List<Author> authors = new ArrayList<>();
+        authorRepository.findAll().forEach(author -> {
+            author.setPassword("");
+            authors.add(author);
+        });
+        return authors;
     }
 }
